043fdb486792244b8f267371f567c77cbd5e3a66
kurau <kurau.mm@gmail.com>
2024-11-03 11:16:41 +0500

    add fix full name listener

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    java
+}
+
+tasks.withType(Wrapper::class) {
+    gradleVersion = "8.7"
+}
+
+group = "org.example.cucumber"
+version = "1.0-SNAPSHOT"
+
+val allureVersion = "2.26.0"
+val cucumberVersion = "7.16.1"
+val aspectJVersion = "1.9.22"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.withType(JavaCompile::class) {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
+
+val agent: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = true
+}
+
+tasks.test {
+    ignoreFailures = true
+    useJUnitPlatform()
+//    jvmArgs = listOf(
+//        "-javaagent:${agent.singleFile}"
+//    )
+}
+
+dependencies {
+    agent("org.aspectj:aspectjweaver:$aspectJVersion")
+
+    testImplementation(platform("io.cucumber:cucumber-bom:$cucumberVersion"))
+    testImplementation("io.cucumber:cucumber-java")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine")
+
+    testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
+    testImplementation("io.qameta.allure:allure-cucumber7-jvm")
+    testImplementation("io.qameta.allure:allure-junit-platform")
+    testImplementation("io.qameta.allure:allure-java-commons:2.29.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.platform:junit-platform-suite")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    testImplementation("org.slf4j:slf4j-simple:2.0.12")
+}
+
+repositories {
+    mavenCentral()
+}
---
+++ src/test/java/org/example/cucumber/allure/FixFullNameListener.java
@@ -0,0 +1,36 @@
+package org.example.cucumber.allure;
+
+import io.qameta.allure.listener.TestLifecycleListener;
+import io.qameta.allure.model.Parameter;
+import io.qameta.allure.model.TestResult;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FixParameterizedFullNameListener implements TestLifecycleListener {
+
+    private static final Pattern PARAMETERIZED_FULL_NAME_MATCHER =
+            Pattern.compile("(?<fullName>.*)(?<lineNumber>:.*)");
+    private static final String FULL_NAME = "%s:%s";
+
+    public void beforeTestStop(final TestResult result) {
+        final Matcher matcher = PARAMETERIZED_FULL_NAME_MATCHER.matcher(result.getFullName());
+
+        System.out.println(matcher.matches());
+        System.out.println(matcher.group("fullName"));
+        System.out.println(result.getName());
+
+        if (matcher.matches()) {
+
+            result.setFullName(
+                    String.format(FULL_NAME, matcher.group("fullName"), result.getName()));
+            if (Objects.isNull(result.getParameters()) || result.getParameters().isEmpty()) {
+                result.setParameters(
+                        Arrays.asList(new Parameter().setName("param")
+                                .setValue(matcher.group("params"))));
+            }
+        }
+    }
+}
